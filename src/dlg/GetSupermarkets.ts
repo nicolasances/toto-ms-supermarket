93a6ac333e94281f1be2eecfb64eab4a39b6e6ae
nicolasances <nicolas.matteazzi@gmail.com>
2024-02-11 18:34:55 +0100

    Get Supermarkets unmocked

--- src/dlg/GetSupermarkets.ts
+++ src/dlg/GetSupermarkets.ts
@@ -2,19 +2,13 @@ import { Request } from "express";
 import { ExecutionContext } from "toto-api-controller/dist/model/ExecutionContext";
 import { TotoDelegate } from "toto-api-controller/dist/model/TotoDelegate";
 import { UserContext } from "toto-api-controller/dist/model/UserContext";
+import { SupermarketStore } from "../store/SupermarketStore";
 
 export class GetSupermarkets implements TotoDelegate {
 
     async do(req: Request, userContext: UserContext, execContext: ExecutionContext): Promise<any> {
 
-        return {
-            supermarkets:
-                [
-                    { name: "Super Brugsen", location: "Solrød Strand" },
-                    { name: "Lidl", location: "Solrød Strand" },
-                    { name: "Føtex", location: "Fisketorvet" },
-                ]
-        }
+        return { supermarkets: new SupermarketStore().getSupermarkets() }
 
     }
 
