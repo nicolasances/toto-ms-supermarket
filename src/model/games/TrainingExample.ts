b7a0bd8c449e29dd0a0d205acfc39da881d11e71
nicolasances <nicolas.matteazzi@gmail.com>
2024-03-24 12:09:42 +0100

    Fixed wrong validation of supermarketId

--- src/model/games/TrainingExample.ts
+++ src/model/games/TrainingExample.ts
@@ -26,7 +26,7 @@ export class TrainingExample {
         if (!req.body.item2) throw new ValidationError(400, `No item2 provided`)
         if (!req.body.label) throw new ValidationError(400, `No label provided`)
         if (req.body.label != "before" && req.body.label != "after") throw new ValidationError(400, "Label must be either 'before' or 'after'")
-        if (req.body.supermarketId) throw new ValidationError(400, `Missing supermarket id`)
+        if (!req.body.supermarketId) throw new ValidationError(400, `Missing supermarket id`)
 
         return new TrainingExample(req.body.item1, req.body.item2, req.body.label, moment().format("YYYYMMDD"), req.body.supermarketId)
     }
