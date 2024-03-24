5da4336a0061ab04f5e341e2e1ff99186ee2713b
nicolasances <nicolas.matteazzi@gmail.com>
2024-03-24 11:53:12 +0100

    Added supermarket id in training example

--- src/model/games/TrainingExample.ts
+++ src/model/games/TrainingExample.ts
@@ -10,12 +10,14 @@ export class TrainingExample {
     item2: string
     label: Label
     date: string
+    supermarketId: string
 
-    constructor(item1: string, item2: string, label: Label, date: string) {
+    constructor(item1: string, item2: string, label: Label, date: string, supermarketId: string) {
         this.item1 = item1;
         this.item2 = item2;
         this.label = label;
         this.date = date;
+        this.supermarketId = supermarketId
     }
 
     static fromHttpRequest(req: Request): TrainingExample {
@@ -24,8 +26,9 @@ export class TrainingExample {
         if (!req.body.item2) throw new ValidationError(400, `No item2 provided`)
         if (!req.body.label) throw new ValidationError(400, `No label provided`)
         if (req.body.label != "before" && req.body.label != "after") throw new ValidationError(400, "Label must be either 'before' or 'after'")
+        if (req.body.supermarketId) throw new ValidationError(400, `Missing supermarket id`)
 
-        return new TrainingExample(req.body.item1, req.body.item2, req.body.label, moment().format("YYYYMMDD"))
+        return new TrainingExample(req.body.item1, req.body.item2, req.body.label, moment().format("YYYYMMDD"), req.body.supermarketId)
     }
 }
 
