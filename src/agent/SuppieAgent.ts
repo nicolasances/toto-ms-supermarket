4c9c49f589b9b1e99f9012a756c7781f7cc84e1e
nicolasances <nicolas.matteazzi@gmail.com>
2026-03-01 13:37:02 +0100

    Suppie Agent with intent recognition

--- src/agent/SuppieAgent.ts
+++ src/agent/SuppieAgent.ts
@@ -4,8 +4,8 @@ import { genkit, z } from "genkit";
 import { ArchivedListStore } from "store/ArchivedListStore";
 import { v4 as uuid } from "uuid";
 import { AgentConversationMessage, GaleConversationalAgent, AgentManifest } from "totoms";
-import { SkillTool } from "./tools/SkillsTools";
-import awsBedrock from "genkitx-aws-bedrock";
+import { AddItemIntent } from "./intents/AddItem";
+import { AddItemToList } from "dlg/AddItemToList";
 
 export class SuppieAgent extends GaleConversationalAgent {
 
@@ -21,6 +21,7 @@ export class SuppieAgent extends GaleConversationalAgent {
 
         const config = this.config as ControllerConfig;
         const streamId = uuid();
+        let streamMessageIndex = 1;
 
         // 1. Get the names from the DB to give the AI an idea of the most common items picked by the user.
         // Instantiate the DB
@@ -34,179 +35,81 @@ export class SuppieAgent extends GaleConversationalAgent {
 
         const ai = genkit({
             plugins: [vertexAI()],
-            model: vertexAI.model('gemini-2.0-flash-lite')
+            model: vertexAI.model('gemini-2.0-flash')
             // plugins: [awsBedrock({ region: "eu-north-1" })],
             // model: "amazon.nova-pro",
         });
 
-        // Setup the tools
-        const skillsToolDef = new SkillTool(names);
-
-        const skillsTool = ai.defineTool(skillsToolDef.definition, async (input: string) => await skillsToolDef.useTool(input));
-
-        // Immediately publish a message to the conversation to let the user know that we have received the message and we are working on it. 
-        // const taskReceivedConfirmation = await ai.generate({
-        //     prompt: "Generate a friendly and informal SHORT message to let the user know that we have received their message and we are working on it. The message should be something like: 'Hey! I got your message and I'm working on adding the items to the shopping list for you!'",
-        //     output: { schema: z.object({ confirmationMessage: z.string() }) }
-        // })
-
-        // this.publishMessage({
-        //     conversationId: message.conversationId,
-        //     messageId: uuid(),
-        //     agentId: message.agentId,
-        //     message: "I'm on it!",
-        //     actor: "agent",
-        //     stream: { streamId, sequenceNumber: 1, last: false }
-        // })
-
-        const entry = await ai.generate({
+        // 1. Intent recognition
+        const intentSchema = z.object({
+            intent: z.string().describe("The intent (among those supported) that best matches the user's message."),
+            feedback: z.string().describe("A few words (short) to the the user that their intent has been understood and that you'll take care of the task.")
+        })
+        const intent = await ai.generate({
+            system: `
+                You are an AI Assistant for an App that manages a user's groceries shopping list. 
+                Your responsibility is, given a user message, to understand its intent. 
+            `,
             prompt: `
-                You are an AI Assistant for a Grocery Shopping List app. 
-
-                These are your skills (capabilities), identified by a name and their description:
-                1. addItems - You can help the user add items to their grocery shopping list
-
-                You have received the following message from the user: ${message.message}.
-
-                Your task is to do the following: 
-                A) check if the user's message is a request that you can fullfill BASED ON YOUR SKILLS (capabilities). 
-                B) Two options: 
-                    - If you can fulfill the user's request, you MUST load the details instructions for the skill you need to use and then you can perform the task. 
-                    - If you cannot fulfill the user's request, you MUST generate a friendly message to the user explaining that you cannot fulfill their request because it's outside of your capabilities.
+                You have received the following message from the user: 
+                ${message.message}
 
                 ## Your task: 
-                Fulfill the user's request if it's within your capabilities, by using the skills (capabilities) that you have.
+                Select, ONLY among the following itents, the intent that is the most appropriate for this user message. These are THE ONLY intents that you support:
+                - addItems - the user wants to add items to his or her shopping list
+                - removeItems - the user wants to remove items from his or her shopping list
+
+                If the user's message does not fit one of these intents, return "noSupportedItent". 
+
+                Also, generate a sentence for the user that makes him or her understand that you have understood their intent. 
             `,
-            tools: [skillsTool],
+            output: {
+                schema: intentSchema
+            }
         })
 
-        console.log(entry.output);
-        
+        this.publishMessage({
+            conversationId: message.conversationId,
+            messageId: uuid(),
+            agentId: message.agentId,
+            message: intent.output?.feedback || "Sorry, I did not understand.",
+            actor: "agent",
+            stream: { streamId, sequenceNumber: streamMessageIndex++, last: false }
+        })
+
+        if (intent.output?.intent === "addItems") {
+
+            // Process the intent 
+            const addItemIntentResult = await new AddItemIntent(config, ai).processIntent(message);
+
+            this.publishMessage({
+                conversationId: message.conversationId,
+                messageId: uuid(),
+                agentId: message.agentId,
+                message: (await ai.generate({
+                    prompt: `
+                        You have just added ${addItemIntentResult.items.length} items to the user's shopping list.
+                        Generate a nice short sentence to let the user know that you have added the items to the shopping list. If no items were added, tell the user so. 
+                    `,
+                    output: { schema: z.object({ confirmationMessage: z.string() }) }
+                })).output?.confirmationMessage || "I have added the items to the shopping list for you!",
+                actor: "agent",
+                stream: { streamId, sequenceNumber: streamMessageIndex++, last: false }
+            })
+
+            // TODO: Add the items to the shopping list in the DB
+
+        }
+        else if (intent.output?.intent === "removeItems") {
+            throw new Error("Not implemented yet");
+        }
 
         return {
             conversationId: message.conversationId,
             messageId: message.messageId,
             agentId: message.agentId,
-            message: entry.output || "Sorry, I cannot fulfill your request because it's outside of my capabilities.",
-            actor: "agent",
-            stream: { streamId, sequenceNumber: 2, last: true }
+            message: "Sorry I cannot help you on this yet.",
+            actor: "agent"
         }
-
-        
-        // const finalOutputSchema = z.object({
-        //     items: z.array(z.string().describe("Name of the item in the grocery shopping list. Only the name.")).describe("List of items in the shopping list")
-        // })
-
-        // // 2.1. First iteration to get the list of items, but not in a structured way, just to let the AI do its best to interpret the user input and correct the mispelled items.
-        // let extractedList = await ai.generate({
-        //     prompt: `
-        //                 You are a grocery shopping expert that knows all typical items that can be found in a supermarket in Denmark and Italy. 
-        //                 You know ALL the names of grocery items in English (main language), Danish and Italian (second languages). 
-        //                 You will receive lists of supermarket items (groceries shopping list) made by a user. 
-        //                 The list is made from a AI-generated transcript of an audio recording where the user recorded items to be bought at the supermarket. 
-        
-        //                 Important: the transcript will MOST LIKELY contain errors as the user uses multiple languages, with the majority of items being dictated in English, but some in Danish and Italian. 
-        //                 The AI that transcribes the audio is english-speaking so Danish (mostly) and Italian (some) items will most likely be mispelled and look like gibberish to you. 
-        
-        //                 Your task is to create the groceries shopping list from the transcript and CORRECT all the mispelled or misinterpreted item names. 
-        
-        //                 These are the most common items picked by the user: ${JSON.stringify(names)}.
-                        
-        //                 Use this list to try to reconcile names whenever you have a doubt. 
-        
-        //                 ## Your task: 
-        //                 Create the groceries shopping list from the following transcription of the user's desires of items to be added to the list: 
-        //                 ${message.message} 
-        //             `,
-        //     output: {
-        //         schema: finalOutputSchema
-        //     }
-        // });
-
-        // // 2. Evaluate and iterate
-        // let attempts = 0;
-        // let evaluation;
-        // let evaluationsHistory = [];
-        // while (attempts < 3) {
-
-        //     // Evaluate the output
-        //     evaluation = await ai.generate({
-        //         prompt: `
-        //                     Evaluate the following list of items extracted from the user's transcription and correct it if you find any mistake. 
-        //                     The list of items is: ${JSON.stringify(extractedList.output?.items)}. 
-                            
-        //                     The user's transcription is the following: ${message.message}. 
-                            
-        //                     Remember that the most common items picked by the user are: ${JSON.stringify(names)}. 
-        
-        //                     ## Your task: 
-        //                     If you find any thing that looks like a mistake, propose a correction and explain why you think it's a mistake. 
-        
-        //                     ## Rules
-        //                     - DO NOT change anything, just propose corrections.
-        //                     - Make sure to double check with the user's transcript, because the previous extractor might have truncated or simplified some item names, removing important terms to understand the context. 
-        //                         *An important example is "Bacon i tern", which might be truncated to "Bacon" but it's actually a specific type of bacon that the user usually buys and that is important to keep in the list.*
-        //                     - Corrections are not mandatory. Only propose a correction if you are really sure that there is a mistake and that you know what the user meant.
-        //                     - If the correction is the same as the corrected (extracted) item name, then it's not a correction and you should not propose it.
-        //                 `,
-        //         output: {
-        //             schema: z.object({
-        //                 items: z.array(z.object({
-        //                     name: z.string().describe("Original name of the item extracted from the user's transcription, with possible mistakes."),
-        //                     correction: z.string().optional().nullable().describe("Proposed correction for the item name, if a mistake was found."),
-        //                     reason: z.string().optional().nullable().describe("Explanation for why the correction was proposed."),
-        //                 }))
-        //             })
-        //         }
-        //     });
-
-        //     evaluationsHistory.push(evaluation.output?.items || []);
-
-        //     // Check if there are any corrections proposed, if not, we can stop the iteration
-        //     const correctionsProposed = evaluation.output?.items.filter(item => item.correction);
-        //     if (!correctionsProposed || correctionsProposed.length === 0) {
-        //         break;
-        //     }
-
-        //     // Refine 
-        //     extractedList = await ai.generate({
-        //         prompt: `
-        //                     The following list of items was extracted from the user's transcription: ${JSON.stringify(extractedList.output?.items)}. 
-        //                     An agent has evaluated this list and proposed the following corrections: ${JSON.stringify(evaluation.output)}.
-                            
-        //                     ## Your task: 
-        //                     Refine the list of items based on the proposed corrections and explanations. 
-        //                     If a correction is proposed and the reason is valid, apply the correction to the item name. 
-        //                 `,
-        //         output: {
-        //             schema: finalOutputSchema
-        //         }
-
-        //     });
-
-        //     attempts++;
-        // }
-
-        // const confirmation = await ai.generate({
-        //     prompt: `Generate a nice sentence to the user confirming that you have added the following number of items to the shopping list: ${extractedList.output?.items.length}. The sentence should be friendly and informal, like something a helpful assistant would say. "`,
-        //     output: { schema: z.object({ confirmationMessage: z.string() }) }
-        // })
-
-        // this.publishMessage({
-        //     conversationId: message.conversationId,
-        //     messageId: uuid(),
-        //     agentId: message.agentId,
-        //     message: confirmation.output?.confirmationMessage || "I have added the items to the shopping list for you!",
-        //     actor: "agent",
-        //     stream: { streamId, sequenceNumber: 2, last: true }
-        // })
-
-        // return {
-        //     conversationId: message.conversationId,
-        //     messageId: message.messageId,
-        //     agentId: message.agentId,
-        //     message: confirmation.output?.confirmationMessage || "I have added the items to the shopping list for you!",
-        //     actor: "agent"
-        // }
     }
 }
