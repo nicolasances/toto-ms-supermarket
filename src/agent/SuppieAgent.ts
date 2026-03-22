dc6b0cb89673d876bbbf181f4bbdaddb75c0ccf9
nicolasances <nicolas.matteazzi@gmail.com>
2026-03-22 11:03:19 +0100

    Updated Model version

--- src/agent/SuppieAgent.ts
+++ src/agent/SuppieAgent.ts
@@ -23,8 +23,8 @@ export class SuppieAgent extends GaleConversationalAgent {
         let streamMessageIndex = 1;
 
         const ai = genkit({
-            plugins: [vertexAI()],
-            model: vertexAI.model('gemini-2.0-flash')
+            plugins: [vertexAI({ location: "europe-west1" })],
+            model: vertexAI.model('gemini-2.0-flash'),
             // plugins: [awsBedrock({ region: "eu-north-1" })],
             // model: "amazon.nova-pro",
         });
@@ -39,8 +39,8 @@ export class SuppieAgent extends GaleConversationalAgent {
                 If the user is naming items to add or remove from the list follow THESE VERY IMPORTANT CONSIDERATIONS; 
                     - The list could come from a speech-to-text transcription and might contain errors. Some items might have been pronounced in Danish or Italian and the translation might be wrong. 
                     - BEFORE adding an item to the list, if the item could be a Danish mispelling, check if you find it in the common supermarket items list. If you find something similar, use that one. 
-            `, 
-            identity: `You are Suppie, an assistant to help users manage their supermarket list.`, 
+            `,
+            identity: `You are Suppie, an assistant to help users manage their supermarket list.`,
             personality: `You are concise but friendly. You always try to keep answers short and to the point.`
         }).loop({ goal: message.message });
 
